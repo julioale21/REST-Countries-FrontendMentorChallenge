e69d76f76e01aa67003cf14ad51f2f0045998996
Julio Romero <julio_ale21@hotmail.com>
2021-07-09 18:02:09 -0300

    chore: used custom components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,54 @@
 import React from "react";
+import { Container, Stack, Input, Select, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import useSearch from "./useSearch";
+import CountriesList from "../../components/CountriesList";
 
 const Home: React.FC = () => {
+  const { countries, search, filter, handleFilterChange, handleInputChange } = useSearch();
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container
+      alignItems="center"
+      fontFamily="'Nunito Sans', sans-serif"
+      maxW={{ base: "container.xl" }}
+    >
+      <Stack
+        alignItems={{ base: "flex-start", md: "center" }}
+        direction={{ base: "column", md: "row" }}
+        justifyContent={{ md: "space-between" }}
+        marginTop={12}
+        spacing={{ base: 6, md: 0 }}
+      >
+        <InputGroup size="lg">
+          <InputLeftElement pointerEvents="none">
+            {" "}
+            <SearchIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            placeholder="Select your country"
+            type="text"
+            value={search}
+            width={{ base: "100%", md: "300px" }}
+            onInput={(e) => handleInputChange(e)}
+          />
+        </InputGroup>
+
+        <Select
+          placeholder="Filter by Region"
+          value={filter}
+          width="200px"
+          onChange={handleFilterChange}
+        >
+          <option value="Africa">Africa</option>
+          <option value="Americas">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europa</option>
+          <option value="Oceania">Oceania</option>
+        </Select>
+      </Stack>
+      <CountriesList countries={countries} />
+    </Container>
   );
 };
 
