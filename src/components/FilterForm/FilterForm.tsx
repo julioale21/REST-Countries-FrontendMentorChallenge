06123f0bfdfe4baaaa45ea5fcc5a6e686737cfb5
Julio Romero <julio_ale21@hotmail.com>
2021-07-10 14:22:07 -0300

    style: changed style based on color mode

--- src/components/FilterForm/FilterForm.tsx
+++ src/components/FilterForm/FilterForm.tsx
@@ -1,3 +1,4 @@
+import { useColorMode } from "@chakra-ui/react";
 import { Select } from "@chakra-ui/select";
 import React from "react";
 
@@ -7,13 +8,19 @@ interface Props {
 }
 
 const FilterForm: React.FC<Props> = ({ filter, handleChange }) => {
+  const { colorMode } = useColorMode();
+
   return (
     <Select
+      _focus={{ ring: "none" }}
+      backgroundColor={colorMode === "light" ? "white" : "darkBlue"}
       className="filter"
+      color={colorMode === "light" ? "gray.700" : "gray.200"}
+      fontWeight="bold"
       placeholder="Filter by Region"
       size="lg"
       value={filter}
-      width="230px"
+      width={{ base: "200px", md: "230px" }}
       onChange={handleChange}
     >
       <option value="Africa">Africa</option>
