611f2f7cd4771439d6b475ad73d49abd4b7056c5
Julio Romero <julio_ale21@hotmail.com>
2021-07-10 11:25:44 -0300

    style: added custom color to theme

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,10 +1,28 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import { extendTheme } from "@chakra-ui/react";
 
-const config: ThemeConfig = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-};
-
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+  colors: {
+    darkBlue: "hsl(209, 23%, 22%)",
+    veryDarkBlueBg: "hsl(207, 26%, 17%)",
+    veryDarkBlueText: "hsl(200, 15%, 8%)",
+    darkGray: "hsl(0, 0%, 52%)",
+    veryLightGray: "hsl(0, 0%, 98%)",
+    white: "hsl(0, 0%, 100%)",
+  },
+  components: {
+    Text: {
+      variants: {
+        custom: (props) => ({
+          color: mode("veryDarkBlueText", "white")(props),
+        }),
+      },
+    },
+  },
+});
 
 export default theme;
