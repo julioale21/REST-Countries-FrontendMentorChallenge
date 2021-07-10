9ee7b5fa7f4348856b8b689efeb0c3affbd13d43
Julio Romero <julio_ale21@hotmail.com>
2021-07-10 11:19:48 -0300

    chore: added app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout/AppLayout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <AppLayout>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </AppLayout>
     </Router>
   );
 }
